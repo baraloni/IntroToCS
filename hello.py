238c176b1566d96c4863b2576847434db789886a
Bar Aloni <48641434+baraloni@users.noreply.github.com>
2021-05-23 11:48:45 +0300

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-##############################################################################################
-# FILE : hello.py
-# WRITER : Bar Aloni , baraloni , 305230740
-# EXERCISE : intro2cs ex0 2016-2017
-# DESCRIPTION : A simple program that prints " Hello World!" to the simple output (screen)
-##############################################################################################
-
-print("Hello World!")
