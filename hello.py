99c0f11b67d699eb004b012996da16be4f889b90
baraloni <bar.aloni@mail.huji.ac.il>
2021-05-23 11:45:02 +0300

    final

---
+++ hello.py
@@ -0,0 +1,8 @@
+##############################################################################################
+# FILE : hello.py
+# WRITER : Bar Aloni , baraloni , 305230740
+# EXERCISE : intro2cs ex0 2016-2017
+# DESCRIPTION : A simple program that prints " Hello World!" to the simple output (screen)
+##############################################################################################
+
+print("Hello World!")
